88236513ac86ce0070ef6851266b24e34ea2a20d
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-04-21 17:34:00 +0100

    Added main.c

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/21 17:33:17 by dda-silv          #+#    #+#             */
+/*   Updated: 2021/04/21 17:33:20 by dda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int	main(void)
+{
+	t_dlist	*cmd_history;
+
+	while (1)
+	{
+		write_prompt();
+		cmd_history->data = (void *)get_cmd();
+		execute_cmd((t_cmd_table *)cmd_history->data);
+	}
+	return (0);
+}
