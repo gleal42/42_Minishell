16e0e744c48b4374bed60f3678408a1bdfb6dffa
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-05-03 19:27:24 +0100

    Started working on the execute

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+        */
+/*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 14:42:15 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/05/03 15:49:05 by gleal            ###   ########.fr       */
+/*   Updated: 2021/05/03 19:18:59 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,32 +80,64 @@ int	execute_cmd_table(t_cmd_table *cmd_table, t_list **env, int	last_status)
 }
 
 /*
-** Executes the command based on the first token/word;
+** Executes the command based on the first token/word
 ** @param:	- [t_cmd *] current command
-**			- [t_list **] Pointer to linked list with all the
-**							environment variables;
-**			- [int] Idenfifies if the output will be redirected or if
-**			we should execute the command.
-** @return:	[int] exit status of command;
+**			- [t_list **] pointer to linked list with all the environment
+						  variables
+**			- [int] idenfifies if the output will be redirected or if we should
+**					execute the command
+** @return:	[int] exit status of command
 ** Line-by-line comments:
-** @7		replaces environment variables by their values;
-** @12		executes recreated functions (echo, cd, pwd, export, unset, env, exit);
+** @7		Replaces environment variables by their values
+** @12		Executes recreated functions (echo, cd, pwd, export, unset, env, exit)
 */
 
 int	execute_cmd(t_cmd *cmd, t_list **env, int pipe, int last_status)
 {
 	t_list	*tokens;
 	char	*first;
+	char	**arr_tokens;
+	char	**arr_env;
 
 	tokens = cmd->tokens;
 	if (tokens == 0)
 		return (0);
 	env_vars(tokens, last_status);
+	arr_tokens = convert_list_to_arr_tokens(tokens);
+	arr_env = convert_list_to_arr(*env);
 	first = ((t_token *)tokens->data)->str;
 	if (ft_strcmp(first, "exit") == 0 && pipe == 0)
 		ft_exit(0);
 	if (is_builtin(first))
 		return (execute_builtin(tokens, env));
 	else
-		return (1);
+		return (execute(arr_tokens, cmd->redirs, arr_env));
+}
+
+int	execute(char **tokens, t_list *redirs, char **envp)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();	
+	if (pid == 0)
+	{
+		printf("\033[0;34m📌 Here in %s line %d\n\033[0m", __FILE__, __LINE__);
+		execve(tokens[0], tokens, envp);
+	}
+	if (pid > 0)
+	{
+		printf("\033[0;34m📌 Here in %s line %d\n\033[0m", __FILE__, __LINE__);
+		pid = wait(&status);
+		if (WIFEXITED(status))
+			printf("The process ended with exit(%d).\n", WEXITSTATUS(status));
+		 if (WIFSIGNALED(status))
+			printf("The process ended with kill -%d.\n", WTERMSIG(status));
+	}
+	if (pid < 0)
+		perror("In fork():");
+	// free_arr((void **)tokens);
+	// free_arr((void **)envp);
+	return (0);
+	(void)redirs;
 }
--- src/execute/execute.h
+++ src/execute/execute.h
@@ -6,7 +6,7 @@
 /*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 14:44:06 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/05/03 17:37:45 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/05/03 18:49:50 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		execute_ast(t_ast **ast, t_list **env);
 int		execute_cmd_table(t_cmd_table *cmd_table,
 			t_list **env, int	last_status);
 int		execute_cmd(t_cmd *cmd, t_list **env, int pipe, int last_status);
+int		execute(char **tokens, t_list *redirs, char **envp);
 
 /*
 ** File execute_utils.c
